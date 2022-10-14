8f3b3b09b311f3aac30a5d331ef6d474f40b68c2
shreyaaashet <shreyashet@kccemsr.edu.in>
2022-10-14 19:14:30 +0530

    piping

--- streams.js
+++ streams.js
@@ -6,9 +6,13 @@ const writeStream = fs.createWriteStream('./docs/item3.txt');
 
 
 // whenver we get a chunk of data we fire an event , on is event listener
-readStream.on('data',(chunk)=>{
-console.log("----new chunk----");
-console.log(chunk);
-writeStream.write("\nnew chuck added below\n");
-writeStream.write(chunk);
-});
+
+// readStream.on('data',(chunk)=>{
+// console.log("----new chunk----");
+// console.log(chunk);
+// writeStream.write("\nnew chuck added below\n");
+// writeStream.write(chunk);
+// });
+
+//piping: readStream data piped into writeStream
+readStream.pipe(writeStream);
