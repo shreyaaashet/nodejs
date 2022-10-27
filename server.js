c444ffb3c4f67fcac066c3c08cd9bddd65102cd3
shreyaaashet <shreyashet@kccemsr.edu.in>
2022-10-27 18:49:38 +0530

    sending html pg to browser

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const _ =require('lodash');
+
+const server = http.createServer((req, res) => {
+//lodash
+const num = _.random(0.20);
+console.log(num)
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+  // runs just once:
+  const greet=_.once(()=>{
+console.log("hello",)
+  });
+ greet();
+
+  // routing
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');// redirect them to about pg
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+  }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    //res.write(data);
+    res.end(data);
+  });
+
+
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
