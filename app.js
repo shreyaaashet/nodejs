e2eea887714ee9d85fac0f7d003a1aa1231fe6bb
shreyaaashet <shreyashet@kccemsr.edu.in>
2022-12-08 13:13:34 +0530

    view engine

--- app.js
+++ app.js
@@ -3,24 +3,34 @@ const express = require("express");
 //express app
 const app=express();
 
+// register view engine
+app.set('view engine','ejs'); // look for view engine inside ejs in this file as root
+
+
+
+
 // listen for requests( here we writing a response to a get req)
 app.listen(3000);
 //get req listen to that url, inside funct
 // req is info of the req like get or post and res can be used to send res 
 app.get('/',(req,res)=>{
-// res.send('<p>hey this is res for home pg</p>');
-res.sendFile('./views/index.html',{root:__dirname}); // mentioning the root is the current directory
-})
+const data=[
+  {title:"shreya",description:"skdkdkdk"},
+  {title:"sa",description:"skdkdkdk"},
+]
+res.render('index',{title:"Home",data});
+});
 
 app.get('/about',(req,res)=>{
-  res.sendFile('./views/about.html',{root:__dirname}); 
-  })
+  res.render('about',{title:"About"});
+  // res.sendFile('./views/about.html',{root:__dirname}); 
+  });
 
-  // redirecting
-app.get('/about-us',(req,res)=>{
-  res.redirect('/about')
+app.get("/blogs/create",(req,res)=>{
+res.render('create',{title:"Create Blogs"});
 })
+
 // 404 pg should always be in end
 app.use((req,res)=>{
-  res.status(404).sendFile('./views/404.html', {root:__dirname})
+  res.status(404).render('404',{title:"Error page"});
 })
